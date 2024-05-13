155d9ce20b96bbeec720e0439e89c9bc4cf05efb
rinick <rinick@gmail.com>
2024-05-12 23:26:50 -0700

    add es

---
+++ es/index.js
@@ -0,0 +1,56 @@
+'use strict'
+
+var _interopRequireDefault =
+  require('@babel/runtime/helpers/interopRequireDefault').default
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+})
+Object.defineProperty(exports, 'Calendar', {
+  enumerable: true,
+  get: function get() {
+    return _Calendar.default
+  },
+})
+Object.defineProperty(exports, 'DateLocalizer', {
+  enumerable: true,
+  get: function get() {
+    return _localizer.DateLocalizer
+  },
+})
+Object.defineProperty(exports, 'Navigate', {
+  enumerable: true,
+  get: function get() {
+    return _constants.navigate
+  },
+})
+Object.defineProperty(exports, 'Views', {
+  enumerable: true,
+  get: function get() {
+    return _constants.views
+  },
+})
+exports.components = void 0
+Object.defineProperty(exports, 'luxonLocalizer', {
+  enumerable: true,
+  get: function get() {
+    return _luxon.default
+  },
+})
+Object.defineProperty(exports, 'move', {
+  enumerable: true,
+  get: function get() {
+    return _move.default
+  },
+})
+var _EventWrapper = _interopRequireDefault(require('./EventWrapper'))
+var _BackgroundWrapper = _interopRequireDefault(require('./BackgroundWrapper'))
+var _Calendar = _interopRequireDefault(require('./Calendar'))
+var _localizer = require('./localizer')
+var _luxon = _interopRequireDefault(require('./localizers/luxon'))
+var _move = _interopRequireDefault(require('./utils/move'))
+var _constants = require('./utils/constants')
+var components = (exports.components = {
+  eventWrapper: _EventWrapper.default,
+  timeSlotWrapper: _BackgroundWrapper.default,
+  dateCellWrapper: _BackgroundWrapper.default,
+})
