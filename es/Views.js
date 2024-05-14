9e5acf78115126829b54ff750a87529fab473b7e
rinick <rinick@gmail.com>
2024-05-13 18:42:26 -0700

    remove Agenda and tests

--- es/Views.js
+++ es/Views.js
@@ -10,7 +10,5 @@ var _constants = require("./utils/constants");
 var _Month = _interopRequireDefault(require("./Month"));
 var _Day = _interopRequireDefault(require("./Day"));
 var _Week = _interopRequireDefault(require("./Week"));
-var _WorkWeek = _interopRequireDefault(require("./WorkWeek"));
-var _Agenda = _interopRequireDefault(require("./Agenda"));
-var VIEWS = (0, _defineProperty2.default)((0, _defineProperty2.default)((0, _defineProperty2.default)((0, _defineProperty2.default)((0, _defineProperty2.default)({}, _constants.views.MONTH, _Month.default), _constants.views.WEEK, _Week.default), _constants.views.WORK_WEEK, _WorkWeek.default), _constants.views.DAY, _Day.default), _constants.views.AGENDA, _Agenda.default);
+var VIEWS = (0, _defineProperty2.default)((0, _defineProperty2.default)((0, _defineProperty2.default)({}, _constants.views.MONTH, _Month.default), _constants.views.WEEK, _Week.default), _constants.views.DAY, _Day.default);
 var _default = exports.default = VIEWS;
